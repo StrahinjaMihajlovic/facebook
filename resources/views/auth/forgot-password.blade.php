cf7572e5cd8a074395ddc59b3a38e47798071b7c
Marko Milojkovic <marko.milojkovic@quantox.com>
2021-04-09 10:44:17 +0200

    Frontend for login,register,forgot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,26 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.loginLayout')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@section('title')
+    Forgot password
+@endsection
+
+@section('content')
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+        <div class="password">
+            <input id="email" type="text" name="email" value="{{ old('email') }}" placeholder="Email address" required autofocus>
         </div>
-
+        <div>
+            <button class="botton" type="botton">Email Password Reset Link</button>
+        </div>
+    </form>
+    <div clas="password" style="padding: 20px;">
+        Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+    </div>
+    <div class="password" style="padding: 20px;">
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
-
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+@endsection
