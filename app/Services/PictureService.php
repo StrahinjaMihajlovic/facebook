26a9c6b8839f243ca0c00bc76613d5c1d33ad756
Strahinja <strahinja.mihajlovic@quantox.com>
2021-04-13 17:06:11 +0200

    Added models, migrations and additional logic for the post pictures

---
+++ app/Services/PictureService.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Http\Requests\PictureRequest;
+use App\Models\Post;
+use App\Models\PostPictures;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class PictureService
+{
+    /**
+     * @param PictureRequest $request
+     * @param Post $post
+     * @return bool
+     */
+    public function store(PictureRequest $request, Post $post){
+
+        if(!$request->picture){
+            return false;
+        }
+
+        $picture = new PostPictures();
+        $location = Storage::disk('images')->put('images/posts', $request->picture);
+
+        if(Storage::disk('images')->exists($location)){
+
+            $picture->file = str_replace('images//posts//', '', $location);
+            $picture->post_id = $post->id;
+            return $picture->save();
+
+        }
+        else{
+            return false;
+        }
+    }
+}
---
+++ database/factories/PostPicturesFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PostPictures;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostPicturesFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = PostPictures::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
