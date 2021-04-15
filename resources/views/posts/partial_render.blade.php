42f14df675a50176c36569eae9f47960ee09b17e
Strahinja <strahinja.mihajlovic@quantox.com>
2021-04-15 11:39:21 +0200

    Added the counting field for likes

--- resources/views/posts/partial_render.blade.php
+++ resources/views/posts/partial_render.blade.php
@@ -44,6 +44,7 @@
             </div>
             <div class="card-footer">
                 <a onclick='likePost(this, {{ $post->id }})'class="card-link"> <i class="fa fa-gittip"></i>
+                    <p style="display:inline-block">{{ count($post->likes) }}</p>
                     <p style="display:inline-block">@if($post->isLiked) Dislike @else Like @endif</p></a>
                 <a style="color:#007bff;cursor: pointer;" class="card-link"> <i class="fa fa-comment" onclick="showComments()" > Comment</i></a>
                 <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a>
