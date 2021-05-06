63ddd11c47f3ee8970b4af50cbf965946ba83197
Strahinja <strahinja.mihajlovic@quantox.com>
2021-05-06 09:53:20 +0200

    Created a command for sending emails to users with unread notification

---
+++ app/Console/Commands/SendEmails.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\NotificationService;
+use Illuminate\Console\Command;
+
+class SendEmails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mail:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends emails to users about unread notifications';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(NotificationService $service)
+    {
+        $service->sendEmailsForUnreadNotifications();
+    }
+}
